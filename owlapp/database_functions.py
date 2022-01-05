19673e8463369a0a3ff7c2a65482dca333023977
Thomas Lardreau <tomwarcraft@yahoo.fr>
2022-01-05 11:26:10 +0100

    finished slack posting func

--- owlapp/database_functions.py
+++ owlapp/database_functions.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-db_name = 'owlappdb'
-db_user = 'username'
-db_pass = 'secret'
-db_host = 'db'
-db_port = '5432'
+load_dotenv()
+
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_host = os.getenv("DB_HOST")
+db_port = str(os.getenv("DB_PORT"))
 
 # Connect to the database
 #db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
